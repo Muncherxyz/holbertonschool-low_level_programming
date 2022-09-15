7d69211ac778ae8e6e3dbb23e2c13bb21c59d034
root <5532@holbertonstudents.com>
2022-09-15 08:58:53 -0700

    ..

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
-#!/bin/bash
-into (void) {
-puts("Programming is like building a multilingual puzzle")
-return (0)
-}
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+    }
