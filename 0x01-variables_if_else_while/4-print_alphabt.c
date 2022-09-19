f10c8930251c747cb6ebe124ff8d3772eb0e767d
root <5532@holbertonstudents.com>
2022-09-19 10:18:00 -0700

    ..

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-  char ch;
+char ch;
 
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    {
-      putchar (ch);
-    }
-  {
-    putchar ('\n');
-  }
-  return (0);
+for (ch = 'a' ; ch <= 'z' ; ch++)
+    
+if (ch != 'e' && ch != 'q')
+{
+putchar (ch);
+}
+{
+putchar ('\n');
+}
+return (0);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+int main (void)
+{
+  int num = "0";
+  for (num = 0; num <= 9; num++)
+    putchar(num);
+  putchar("%d, " num)
+      
+}
