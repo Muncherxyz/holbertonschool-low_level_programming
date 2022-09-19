1904d1b1e0a9735f9a11a374fb2e80ce7033a492
root <5532@holbertonstudents.com>
2022-09-19 09:17:50 -0700

    ..

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+int num = '0';
+for (num = '0'; num <= '9'; num++)
+putchar(num);
+char ch;
+for (ch = 'e' ; ch <= 'f' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+  
+}
