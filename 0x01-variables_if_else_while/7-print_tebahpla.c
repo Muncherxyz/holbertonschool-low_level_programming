29c0cd31bd9be93ea4377c3fdd490b246d614fae
root <5532@holbertonstudents.com>
2022-09-19 09:05:11 -0700

    ..

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- *
+ * Return: 0 (Success)
  *
  */
 int main(void)
 {
 char ch;
 
-for (ch <= 'z' ; ch = 'a' ; ch--)
+for (ch = 'z' ; ch <= 'a' ; ch--)
 {
 putchar (ch);
 }
