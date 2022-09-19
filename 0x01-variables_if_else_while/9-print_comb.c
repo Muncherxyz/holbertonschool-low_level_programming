99b706ba20302bade90463caf4e1772a6da280c2
root <5532@holbertonstudents.com>
2022-09-19 11:58:08 -0700

    ..

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
  *
- *
+ * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int x;
 for (x = '0'; x <= '9'; x++)
@@ -17,6 +18,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-return(0);
+return (0);
 }
-
