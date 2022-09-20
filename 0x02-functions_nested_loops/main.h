d61b4da30aa644540e76d7d1ea5fafcfad179140
root <5532@holbertonstudents.com>
2022-09-20 08:19:21 -0700

    ..

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <unistd.h>
+
 int main(void);
-/** */
+/** prototype file for last project */
 int _putchar(char c);
 void print_alphabet(void);
+int _islower(int c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
