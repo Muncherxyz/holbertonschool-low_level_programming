245644a102da76a34eff6e92d739c73522708d46
root <5532@holbertonstudents.com>
2022-09-22 08:48:31 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *
+ *checks if the letter is uppercase
+ *
+ *
+ */
+int _isupper(int c);
+{
+  if ((c>="A") && (c<="Z")
+      return (1);
+      else
+	return (0);
+
+}
+
+
