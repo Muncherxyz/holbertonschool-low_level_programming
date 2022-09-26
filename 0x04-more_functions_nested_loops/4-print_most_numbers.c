3a52fa42bb40a12c119e6053120944bc49b84caa
root <5532@holbertonstudents.com>
2022-09-26 09:05:18 -0700

    ..

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,15 @@
 /**
  * print_most_numbers - will print all num except 2&4
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
-  int i;
-  for( i = 0; i < 10; i++);
-  {
-    if (x != 2 && x != 4)
-      _putchar(x + '0');
-  }
-  _putchar('\n');
- 
+int i;
+for( i = 0; i < 10; i++);
+{
+if (x != 2 && x != 4)
+_putchar(x + '0');
+}
+_putchar('\n');
+
   
 }
