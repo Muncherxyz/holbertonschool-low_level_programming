e9ed437febfdba900681401d3314120fb870527b
root <5532@holbertonstudents.com>
2022-09-27 15:10:20 -0700

    ..

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts_half - function that puts half a string
  * @str: parameter
@@ -6,14 +7,14 @@
  */
 void puts_half(char *str)
 {
-int x, y;
+  int x, y, i;
 
 x = strlen(str);
-if (x % 2 ==1)
+if (x % 2 == 1)
 y = x / 2 + 1;
 else
 y = x / 2;
-for (i = y; i < x; ++)
+for (i = y; i < x; i++)
 _putchar(str[i]);
 _putchar('\n');
       
