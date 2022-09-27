c9af4b2cc4b58118dfd687c0a3745700ee3721f9
root <5532@holbertonstudents.com>
2022-09-27 11:59:28 -0700

    ..

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,17 +5,17 @@
  */
 void print_rev(char *s)
 {
-  int i, n;
+int i, n;
 
-  n = 0;
-  while (s[n] != '\0')
-    n++;
+n = 0;
+while (s[n] != '\0')
+n++;
 
-  for (i = n - 1; i >= 0; i--)
-    {
-      _putchar(s[i]);
-    }
-  _putchar('\n');
+for (i = n - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 
 
 }
