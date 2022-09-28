4c657e23485e2cbb95f1904af17e65daf5dce365
root <5532@holbertonstudents.com>
2022-09-27 17:03:25 -0700

    ..

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,10 +5,9 @@
  */
 void print_diagonal(int n);
 {
-  int l;
-  for(l = '0'; c <= n; l++)
-    _putchar ('\');
-  if(n <=0);
-  putchar ('\n')
-
+int l;
+for(l = '0'; c <= n; l++)
+_putchar('\');
+if(n <=0);
+_putchar('\n')
 }
