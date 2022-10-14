dc0be6673035b3f5d1e1383f74fa3e5ca125b5ab
root <5532@holbertonstudents.com>
2022-10-14 10:52:07 -0700

    ..

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
