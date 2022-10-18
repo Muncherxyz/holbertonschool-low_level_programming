4c6d19c444e87d9692cd35f46478d5edc6114b7d
root <5532@holbertonstudents.com>
2022-10-18 12:44:44 -0700

    ..

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - 
+ * malloc_checked - returns a pointer to the allocated memory
  *
+ * @b: parameter
  *
  */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
   void *p;
   
