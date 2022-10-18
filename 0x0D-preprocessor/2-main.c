b78534e072972eb4666d7a64e7d71bb6fc5df04b
root <5532@holbertonstudents.com>
2022-10-17 17:19:57 -0700

    ..

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints name of file it was compiled from followed by a new line
- *
- * return: 0 on success
- */
+* main - prints name of file it was compiled from followed by a new line
+*
+* Return: 0 on success
+*/
 int main(void)
 {
-  printf("%s\n", __FILE__);
-  return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
