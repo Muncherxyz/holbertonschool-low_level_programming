b62b0f32923b3519aa6b41d0bb4a5f02495bee9e
root <5532@holbertonstudents.com>
2022-10-18 17:45:35 -0700

    ..

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,23 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _calloc - function that copies allocation
  *
+ * @nmemb: param
+ * @size: param
  *
- *
+ * Return: P
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *p;
-  unsigned int i;
-
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  p = malloc(nmemb * size);
-  if (p == NULL)
-    return (NULL)
-  for (i = 0; i < (nmemb * size); i++)
-    p[i] = 0;
-  return (p);
-
-  
+char *p;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
+return (p);
 }
