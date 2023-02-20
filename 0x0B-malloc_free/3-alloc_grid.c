7013ce9d168f02a1f810e67d27431cabd95db069
Muncherxyz <5532@holbertonstudents.com>
2023-02-20 10:53:04 -0800

    ..

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,35 +8,35 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int freenum;
-  int x;
-  int y;
-  int i;
-  int **arr;
-  if (width < 1 || height < 1)
-    return (NULL);
-  arr = malloc(sizeof(int *) * height);
-  if (arr == NULL)
-    return (NULL);
-  for (i = 0; i < height; i++)
-    {
-      arr[i] = malloc(sizeof(int) * width);
-      if (arr[i] == NULL)
-	{
-	  for (freenum = 0; freenum < i; freenum++)
-	    {
-	      free(arr[freenum]);
-	    }
-	  free(arr);
-	  return (NULL);
-	}
-    }
-  for (x = 0; x < height; x++)
-    {
-      for (y = 0; y < width; y++)
-	{
-	  arr[x][y] = 0;
-	}
-    }
-  return (arr);
+int freenum;
+int x;
+int y;
+int i;
+int **arr;
+if (width < 1 || height < 1)
+return (NULL);
+arr = malloc(sizeof(int *) * height);
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+arr[i] = malloc(sizeof(int) * width);
+if (arr[i] == NULL)
+{
+for (freenum = 0; freenum < i; freenum++)
+{
+free(arr[freenum]);
+}
+free(arr);
+return (NULL);
+}
+}
+for (x = 0; x < height; x++)
+{
+for (y = 0; y < width; y++)
+{
+arr[x][y] = 0;
+}
+}
+return (arr);
 }
