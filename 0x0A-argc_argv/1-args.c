993959a29e7db0c08aa1318aa209dfddd7142321
Muncherxyz <5532@holbertonstudents.com>
2023-02-20 10:32:33 -0800

    ..

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
-/**
- * main - this is a function to print its name
- * @argc: argc parameter
- * @argv: an array of a command listed
- * Return: 0 for success
- */
-int main(int argc, char *argv[] __attribute__((unused)))
-{
-  printf("%d\n" , argc - 1);
-  return (0);
+#include <stdio.h>                                                                                                                   
+#include "main.h"                                                                                                                    
+/**                                                                                                                                  
+* main - print the name of the program                                                                                              
+* @argc: Count arguments                                                                                                            
+* @argv: Arguments                                                                                                                  
+*                                                                                                                                   
+* Return: Always 0 (Success)                                                                                                        
+*/                                                                                                                                  
+int main(int argc, char *argv[])                                                                                                     
+{                                                                                                                                    
+(void) argv;                                                                                                        
+printf("%i\n", argc - 1);                                                                                                            
+                                                                                                                                     
+return (0);                                                                                                                          
 }
+
