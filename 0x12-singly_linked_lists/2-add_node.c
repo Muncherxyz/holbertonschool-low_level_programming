74d014091683557eff4205d97d14dc24fc81e21e
Muncherxyz <5532@holbertonstudents.com>
2023-03-19 13:31:03 -0700

    .

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,9 +4,16 @@
  *
  *
  */
-list_t *add_node(list_t **head, const char *str){
+list_t *add_node(list_t **head, const char *str)
+{
 
+  list_t *temp, *new;
+  int i;
 
+  temp->str = strdup(str);
+  new = *head;
+
+  
 
 
 
