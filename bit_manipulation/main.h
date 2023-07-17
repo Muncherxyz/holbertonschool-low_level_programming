5cd4f66d76edfbc1d46bfe19c83260a01de6c9c0
Muncherxyz <19kortlanblocker@gmail.com>
2023-07-17 16:20:53 -0700

    fini

---
+++ bit_manipulation/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*
+ * libraries - to be included
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/*
+ * prototypes - function protos below
+ */
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
